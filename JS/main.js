f6d0726368c4b1872c5420baf7ee0185808d72f4
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-08 08:17:03 +0800

    update herE

--- JS/main.js
+++ JS/main.js
@@ -3,5 +3,6 @@
 function btnSubmit() {
 
   const fName = document.getElementById("fName").value ;
-  
+  const mName = document.getElementById("mName").value ;
+  const lName = document.getElementById("lName").value ;
 }
