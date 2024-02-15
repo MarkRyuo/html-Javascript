889c0d7e72f97f37e60a1df214d5da44ce0e73b0
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-15 17:42:12 +0800

    udpate herE

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,10 @@
 
 
-let question = window.prompt("Are you Gay bro ?") ;
 
-do 
+
+do {
+
+  let question = window.prompt("Are you Gay bro ?") ;
+  question = Boolean(question) ;
+  window.alert("Your Gayyyyyy bro ");
+}while(question == True )
