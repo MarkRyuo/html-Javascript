2ed8cc9de1a23b4ce61fa8a443b6fba8386cc226
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-15 17:38:17 +0800

    udpaet herE

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,4 @@
 
 
-function btnSubmit() {
-
-  const fName = document.getElementById("fName").value ;
-  const mName = document.getElementById("mName").value ;
-  const lName = document.getElementById("lName").value ;
-  const age = document.getElementById("age").value ;
-  const dOfBirth = document.getElementById("dOfBirth").value ;
-  const address = document.getElementById("address").value ;
-  const cNumber = document.getElementById("cNumber").value ;
-  const eMail = document.getElementById("eMail").value ;
-
-  fName = String(fName) ;
-  mName = String(mName) ;
-  lName = String(lName) ;
-
   
 
-  
-}
